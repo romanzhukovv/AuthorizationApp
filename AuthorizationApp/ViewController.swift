09f81c730e43130fa7d63cde842b98fcc4cace14
romanzhukovv <zhukov.roman89@gmail.com>
2021-12-14 08:31:16 +0300

    Initial Commit

---
+++ AuthorizationApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AuthorizationApp
+//
+//  Created by Roman Zhukov on 14.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
