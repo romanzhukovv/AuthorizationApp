0fe2151cf451f00e027915f02945221a4c1f8ca6
romanzhukovv <zhukov.roman89@gmail.com>
2021-12-18 17:05:45 +0300

    added transition data to other view

---
+++ AuthorizationApp/ViewControllers/AboutMeViewController.swift
@@ -0,0 +1,16 @@
+//
+//  AboutMeViewController.swift
+//  AuthorizationApp
+//
+//  Created by Roman Zhukov on 18.12.2021.
+//
+
+import UIKit
+
+class AboutMeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
--- AuthorizationApp/ViewControllers/LoginViewController.swift
+++ AuthorizationApp/ViewControllers/LoginViewController.swift
@@ -15,18 +15,26 @@ class LoginViewController: UIViewController {
     @IBOutlet var forgotUserNameButton: UIButton!
     @IBOutlet var forgotPasswordButton: UIButton!
     
-    private let trueUserName = "User"
-    private let truePassword = "12345"
+    let someUser = User.getUser()
     
     override func viewWillLayoutSubviews() {
         fillGradient(colorOne: UIColor.systemOrange, colorTwo: UIColor.systemPurple)
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        let welcomeVC = segue.destination as! WelcomeViewController
+        let tabBarController = segue.destination as! UITabBarController
+        let viewControllers = tabBarController.viewControllers!     //  Не понимаю почему тут восклицательный знак
         
-        if let userName = userNameTF.text {
-            welcomeVC.userName = userName
+        for viewController in viewControllers {
+            if let welcomeVC = viewController as? WelcomeViewController {
+                if let userName = userNameTF.text {
+                    welcomeVC.userName = userName
+                }
+            } else if let navigationVC = viewController as? UINavigationController {
+                let aboutUserVC = navigationVC.topViewController as! AboutMeViewController
+                aboutUserVC.navigationItem.title = someUser.person.name
+                aboutUserVC.navigationItem.title?.append(" \(someUser.person.surname)")
+            }
         }
     }
     
@@ -48,7 +56,7 @@ class LoginViewController: UIViewController {
         case forgotPasswordButton:
             showAlert(title: "Your password", message: "12345")
         default:
-            guard userNameTF.text == trueUserName, passwordTF.text == truePassword else {
+            guard userNameTF.text == someUser.userName, passwordTF.text == someUser.password else {
                 showAlert(title: "Error", message: "Wrong user name or password!")
                 return
             }
@@ -66,7 +74,7 @@ extension LoginViewController {
         present(alert, animated: true)
     }
     
-    private func fillGradient(colorOne: UIColor, colorTwo: UIColor) {
+     private func fillGradient(colorOne: UIColor, colorTwo: UIColor) {
         let gradiendLayer = CAGradientLayer()
         gradiendLayer.frame = view.bounds
         gradiendLayer.colors = [colorOne.cgColor, colorTwo.cgColor]
